4ab9ebe7d9d5c63efff201b5721e2ce542afca5a
Kevin-zwy <92591394+Kevin-zwy@users.noreply.github.com>
2024-10-22 21:46:48 +0800

    Update manage.py

--- myproject/manage.py
+++ myproject/manage.py
@@ -3,6 +3,88 @@
 import os
 import sys
 
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torchvision import datasets, transforms, models
+from torch.utils.data import DataLoader
+from torchvision.datasets import ImageFolder
+# from google.colab import files
+import torch
+import torchvision.models as models
+import torch
+from PIL import Image
+from torchvision import transforms
+class CustomResNet50(nn.Module):
+    def __init__(self, num_classes):
+        super(CustomResNet50, self).__init__()
+        # load ResNet50
+        self.base_model = models.resnet50(weights='IMAGENET1K_V1')
+
+
+        self.base_model.fc = nn.Identity()
+
+
+        self.fc1 = nn.Linear(2048, 512)
+        self.fc2 = nn.Linear(512, num_classes)
+
+
+        self.relu = nn.ReLU()
+
+    def forward(self, x):
+        x = self.base_model(x)
+        x = self.relu(self.fc1(x))
+        x = self.fc2(x)
+        return x
+
+
+
+class CustomDenseNet121(nn.Module):
+    def __init__(self, num_classes):
+        super(CustomDenseNet121, self).__init__()
+        # load DenseNet121
+        self.base_model = models.densenet121(weights='IMAGENET1K_V1')
+
+        # Remove the classification layer
+        self.base_model.classifier = nn.Identity()
+
+        self.fc1 = nn.Linear(1024, 512)
+        self.fc2 = nn.Linear(512, num_classes)
+
+        self.relu = nn.ReLU()
+
+    def forward(self, x):
+        x = self.base_model(x)
+        x = self.relu(self.fc1(x))
+        x = self.fc2(x)
+        return x
+
+
+class CustomMobileNetV2(nn.Module):
+    def __init__(self, num_classes):
+        super(CustomMobileNetV2, self).__init__()
+        # load MobileNetV2
+        self.base_model = models.mobilenet_v2(weights='IMAGENET1K_V1')
+
+
+        self.base_model.classifier = nn.Identity()
+
+
+        self.fc1 = nn.Linear(1280, 512)
+        self.fc2 = nn.Linear(512, num_classes)
+
+
+        self.relu = nn.ReLU()
+
+    def forward(self, x):
+        x = self.base_model(x)
+        x = self.relu(self.fc1(x))
+        x = self.fc2(x)
+        return x
+
+
+
+
 
 def main():
     """Run administrative tasks."""
@@ -15,8 +97,10 @@ def main():
             "available on your PYTHONPATH environment variable? Did you "
             "forget to activate a virtual environment?"
         ) from exc
+
     execute_from_command_line(sys.argv)
 
 
 if __name__ == "__main__":
     main()
+
